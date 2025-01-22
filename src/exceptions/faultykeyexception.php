4a1137338957bd5f683dd99050be62bde03655fc
Luc Guerraz <55530568+lucguerraz@users.noreply.github.com>
2025-01-22 19:41:47 +0100

    fix: resolve php8.4 deprecation notice for implicit null marking

--- src/exceptions/faultykeyexception.php
+++ src/exceptions/faultykeyexception.php
@@ -20,7 +20,7 @@ class faultykeyexception extends Exception
     public function __construct(
         string $key = '',
         int $code = 0,
-        Exception $previous = null
+        Exception|null $previous = null
     ) {
         parent::__construct(
             "The {$key} env variable is malformed.",
--- src/exceptions/missingkeyexception.php
+++ src/exceptions/missingkeyexception.php
@@ -20,7 +20,7 @@ class missingkeyexception extends Exception
     public function __construct(
         string $key = '',
         int $code = 0,
-        Exception $previous = null
+        Exception|null $previous = null
     ) {
         parent::__construct(
             "Could not find the {$key} env variable for WPML.",
